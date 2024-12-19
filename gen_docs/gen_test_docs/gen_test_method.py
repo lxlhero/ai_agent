328c50537c6f04ef59fb11067bc1dc2cd0ee0f15
Templar0911 <hscwoer@qq.com>
2024-12-19 20:59:37 +0800

    结构化测试方案关键信息并已md格式输出至文件

--- gen_docs/gen_test_docs/gen_test_method.py
+++ gen_docs/gen_test_docs/gen_test_method.py
@@ -19,12 +19,15 @@ import traceback
 from openpyxl import Workbook
 from openpyxl.utils.dataframe import dataframe_to_rows
 from openpyxl.styles import Alignment
+from pydantic import BaseModel, Field
+import pathlib
 
-# 根据产品结束和采购报价excel生成页面级别的功能清单
+
+folder = pathlib.Path(__file__).parent.resolve()
+print(f'当前python文件所在目录：{folder}')
 
 # 从环境变量中获取 OPENAI_API_KEY
 openai_api_key = os.getenv("OPENAI_API_KEY")
-
 if openai_api_key is None:
     raise ValueError("OPENAI_API_KEY environment variable is not set")
 else:
@@ -32,6 +35,12 @@ else:
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
+
+
+# class TestMethodKeyInfo(BaseModel):
+#     test_range: str = Field(description="测试范围，包含具体内容")
+#     function_test: str = Field(description="功能测试，包含具体内容")
+#     key_function_test_case: str = Field(description="关键功能测试用例示例，每个用例包含描述、前置条件、测试步骤、预期结果")
 
 class DataPreprocessingModuleSpecGenerator:
     def __init__(self, overview_path, feature_excel_path):
@@ -77,22 +86,24 @@ class DataPreprocessingModuleSpecGenerator:
         return json_data.to_json(orient='records', force_ascii=False)
 
     def process(self):
+        temperature = 0
         self.llm = ChatOpenAI(model="gpt-4o-mini")
+        # structured_llm = self.llm.with_structured_output(TestMethodKeyInfo)
 
         # 读取产品简述
         with open(self.overview_path, 'r', encoding='utf8') as f:
             product_description = f.read()
 
-        with open('/Users/yangbo/projects/llm/docreator/ai_agent/gen_docs/gen_test_docs/sample_test_method.txt', 'r', encoding='utf8') as f:
+        with open(f'{folder}/sample_test_method.txt', 'r', encoding='utf8') as f:
             sample_test_method = f.read()
 
-        with open('/Users/yangbo/projects/llm/docreator/ai_agent/gen_docs/gen_test_docs/sample_test_record.txt', 'r', encoding='utf8') as f:
+        with open(f'{folder}/sample_test_record.txt', 'r', encoding='utf8') as f:
             sample_test_report = f.read()
 
         # 读取采购报价单excel
         product_details = self.excel_to_json()
 
-        template = PromptTemplate.from_file("/Users/yangbo/projects/llm/docreator/ai_agent/gen_docs/gen_test_docs/product_test.txt")
+        template = PromptTemplate.from_file(f"{folder}/template_prompt.txt")
         prompt = template.format(
             product_description=product_description,
             product_details=product_details,
@@ -103,6 +114,22 @@ class DataPreprocessingModuleSpecGenerator:
         # 使用LLM生成JSON
         result = self.llm.invoke(prompt)
         print(f"llm生成: {result.content}")
+
+        # 解析结果中测试范围、功能测试、关键功能测试用例示例
+        test_range = result.content.split("# 测试范围")[1].split("# 功能测试")[0].strip()
+        function_test = result.content.split("# 功能测试")[1].split("# 关键功能测试用例示例")[0].strip()
+        key_function_test_case = result.content.split("# 关键功能测试用例示例")[1].strip()
+
+        print(f"测试范围: {test_range}")
+        print(f"功能测试: {function_test}")
+        print(f"关键功能测试用例示例: {key_function_test_case}")
+
+        # 读取template_test_method.txt的内容，并且将test_range、function_test、key_function_test_case替换到其中，并写入到result.md文件中
+        with open(f'{folder}/template_test_method.txt', 'r', encoding='utf8') as f:
+            template_test_method = f.read()
+        with open(f'{folder}/result.md', 'w', encoding='utf8') as f:
+            f.write(template_test_method.replace("{test_range}", test_range).replace("{function_test}", function_test).replace("{key_function_test_case}", key_function_test_case))
+
         # try:
         #     # Strip leading/trailing whitespace and newlines
         #     stripped_response = func_names_json_response.strip().strip('```json')
@@ -133,7 +160,7 @@ if __name__ == "__main__":
     # Define the list of platforms
 
     # Path to the product overview
-    overview_path = "/Users/yangbo/projects/llm/docreator/ai_agent/gen_docs/gen_test_docs/overview.txt"
+    overview_path = f"{folder}/overview.txt"
     feature_excel_path = "/Users/yangbo/projects/llm/docreator/ai_agent/featurelist/基于机器学习的民航指挥智能态势感知系统实施项目功能/功能清单.xlsx"
 
     generator = DataPreprocessingModuleSpecGenerator(
